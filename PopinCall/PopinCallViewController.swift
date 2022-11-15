eac626f2efe85ae98532ed4654087b78af247102
Ashwin <ashwin@springr.in>
2022-11-15 21:37:04 +0530

    base

---
+++ PopinCall/PopinCallViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PopinCallViewController.swift
+//  PopinCall
+//
+//  Created by Ashwin Nath on 15/11/22.
+//
+
+import UIKit
+
+public class PopinCallViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    public init() {
+        super.init(nibName: "PopinCallViewController", bundle: Bundle(for: PopinCallViewController.self))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+ 
+
+}
--- PopinCallExamples/ViewController.swift
+++ PopinCallExamples/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PopinCall
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeCall(_ sender: Any) {
+        present(PopinCallViewController(), animated: true, completion: nil)
+    }
+    
 }
 
